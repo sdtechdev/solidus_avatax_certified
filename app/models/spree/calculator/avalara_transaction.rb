3c087d613887d36e710f1b7ad0b468c94515b327
Ashish Tajane <ashishtajane@gmail.com>
2019-07-31 17:09:06 +0530

    Update ship address cache key
    We just need to check zipcode and address
    No need to worry about other fields

--- app/models/spree/calculator/avalara_transaction.rb
+++ app/models/spree/calculator/avalara_transaction.rb
@@ -61,7 +61,7 @@ module Spree
 
     def long_cache_key(order)
       key = order.avatax_cache_key
-      key << (order.ship_address.try(:cache_key) || order.bill_address.try(:cache_key)).to_s
+      key << order.ship_address.avatax_cache_key if order.ship_address
       order.line_items.each do |line_item|
         key << line_item.avatax_cache_key
       end
